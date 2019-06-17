8e8a32b39371904e5c7c7cb0bebd9529ed31c225
FE-TianYongGang <tianyonggang@aibao.com>
2019-06-17 16:44:33 +0800

    :wrench: 修改配置文件

--- index.js
+++ index.js
@@ -8,14 +8,16 @@ module.exports = postcss.plugin('postcss-cubeui-transform', function (opts) {
   // Work with options here
   return function (css) {
     css.walkRules(function (rule) {
-      rule.walkDecls(function(decl) {
-        var reg = /\d+px/gi
-        decl.value = decl.value.replace(reg, function (val) {
-          val = val.toLowerCase()
-          if (val === '1px') return val
-          return parseFloat(val) * 2 + 'px'
+      if (css.source.input.file.match('vant')) {
+        rule.walkDecls(function(decl) {
+          var reg = /\d+px/gi
+          decl.value = decl.value.replace(reg, function (val) {
+            val = val.toLowerCase()
+            if (val === '1px') return val
+            return parseFloat(val) * 2 + 'px'
+          })
         })
-      })
+      }
     })
   }
 })
