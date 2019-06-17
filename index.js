f5222cf91308fc65daf8cb2ff6bb0cb597c8f927
FE-TianYongGang <tianyonggang@aibao.com>
2019-06-17 14:08:56 +0800

    :wrench: 修改配置文件

--- index.js
+++ index.js
@@ -5,20 +5,18 @@ module.exports = postcss.plugin('postcss-cubeui-transform', function (opts) {
     include: /(cube-ui)/,
     exclude: /(src)/
   }
-
   // Work with options here
   return function (css) {
-    css.walkDecls(function (decl) {
-      if (opts.exclude && css.source.input.file && css.source.input.file.match(opts.exclude) !== null) return;
-      // 只处理cube-ui目录
-      if (css.source.input.file.match(opts.include) !== null) {
+    css.walkRules(function (rule) {
+      rule.walkDecls(function(decl) {
         var reg = /\d+px/gi
         decl.value = decl.value.replace(reg, function (val) {
+          console.log(`valvalvalvalvalvalvalvalvalvalvalvalvalvalvalvalvalvalvalvalval: ${val}`)
           val = val.toLowerCase()
           if (val === '1px') return val
           return parseFloat(val) * 2 + 'px'
         })
-      }
+      })
     })
   }
 })
